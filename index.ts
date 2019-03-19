84389a8244c0340ddc6b47d0a77cac6fb1ee6940
Aditya Prabhu <aditya.prabhu@tavant.com>
2019-03-19 20:22:29 +0530

    basic changes

---
+++ index.ts
@@ -0,0 +1,3 @@
+export { AppModule } from 'src/app/app.module';
+export { BangaloreComponent } from 'src/app/bangalore/bangalore.component';
+export { MysoreComponent } from 'src/app/mysore/mysore.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BangaloreComponent } from './bangalore/bangalore.component';
+import { MysoreComponent } from './mysore/mysore.component';
+
+const appRoutes: Routes = [
+  { path: 'city/Bangalore', component: BangaloreComponent },
+  { path: 'city/Mysore', component: MysoreComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BangaloreComponent,
+    MysoreComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
